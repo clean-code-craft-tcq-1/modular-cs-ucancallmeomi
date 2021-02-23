91962543326b9c05f9aab494ceff02fc4948fb0f
Om Prakash <ucancallmeomi@outlook.com>
2021-02-23 15:05:16 +0530

    Splitted the file into smaller pieces of code. Added new function to print the color code reference manual.

---
+++ TelCo.ColorCoder/ColorPair.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace TelCo.ColorCoder
+{
+    class ColorPair : Colours
+    {
+        internal Color majorColor, minorColor;
+
+        public override string ToString()
+        {
+            return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
+        }
+
+        public static ColorPair GetColorFromPairNumber(int pairNumber)
+        {
+            int minorSize = colorMapMinor.Length;
+            int majorSize = colorMapMajor.Length;
+            if (pairNumber < 1 || pairNumber > minorSize * majorSize)
+            {
+                throw new ArgumentOutOfRangeException(
+                    string.Format("Argument PairNumber:{0} is outside the allowed range", pairNumber));
+            }
+
+            int zeroBasedPairNumber = pairNumber - 1;
+            int majorIndex = zeroBasedPairNumber / minorSize;
+            int minorIndex = zeroBasedPairNumber % minorSize;
+
+            ColorPair pair = new ColorPair()
+            {
+                majorColor = colorMapMajor[majorIndex],
+                minorColor = colorMapMinor[minorIndex]
+            };
+
+            return pair;
+        }
+
+
+        public static int GetPairNumberFromColor(ColorPair pair)
+        {
+            int majorIndex = getMajorIndex(pair);
+            int minorIndex = getMinorIndex(pair);
+
+            if (majorIndex == -1 || minorIndex == -1)
+            {
+                throw new ArgumentException(
+                    string.Format("Unknown Colors: {0}", pair.ToString()));
+            }
+
+            return (majorIndex * colorMapMinor.Length) + (minorIndex + 1);
+        }
+
+        public static void ReferenceManual()
+        {
+            for (int i=1; i<=25; i++)
+            {
+                ColorPair colors = GetColorFromPairNumber(i);
+
+                Console.WriteLine("Pair: " + i +", Colors: " + colors.majorColor + ", " + colors.minorColor );
+            }
+                
+        }
+
+    }
+}
---
+++ TelCo.ColorCoder/Colours.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace TelCo.ColorCoder
+{
+   
+    class Colours
+    {
+        
+        public static Color[] colorMapMajor, colorMapMinor;
+       
+     
+        static Colours()
+        {
+            colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
+            colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
+        }
+
+        public static int getMajorIndex(ColorPair pair)
+        {
+            int majorIndex = -1;
+            for (int i = 0; i < colorMapMajor.Length; i++)
+            {
+                if (colorMapMajor[i] == pair.majorColor)
+                {
+                    majorIndex = i;
+                    break;
+                }
+            }
+
+            return majorIndex;
+        }
+
+        public static int getMinorIndex(ColorPair pair)
+        {
+            int minorIndex = -1;
+            for (int i = 0; i < colorMapMinor.Length; i++)
+            {
+                if (colorMapMinor[i] == pair.minorColor)
+                {
+                    minorIndex = i;
+                    break;
+                }
+            }
+
+            return minorIndex;
+        }
+
+
+
+    }
+}
